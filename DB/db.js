0d36c8dd65ab9d86cb4cc1bb1a2301ac84b66919
Aaron Kandle <akandle@gmail.com>
2015-12-29 14:49:23 -0800

    Incremental fixes for Travis CI

--- DB/db.js
+++ DB/db.js
@@ -1,6 +1,6 @@
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('postgres://admin:test@localhost:5432/likedb');
+var sequelize = new Sequelize('postgres://admin2:test@localhost:5432/likedb');
 
 console.log('Database connected.');
 
