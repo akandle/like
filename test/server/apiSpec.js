7b5e95e92596041877494c9a8143485c1d4154a6
Aaron Kandle <akandle@gmail.com>
2015-12-29 12:05:59 -0800

    Implement Travis CI testing integration

--- test/server/apiSpec.js
+++ test/server/apiSpec.js
@@ -1,66 +1,141 @@
+/*jshint expr: true*/
 var expect = require('chai').expect;
 var sinon = require('sinon');
 var request = require('supertest');
 var server = require('../../server/server.js');
 
-describe('API', function() {
-  describe('GET api/profile/:id', function () {
-    it('should respond with a profile object and a 200 status', function (done) {
-      request(server)
-        .get('/api/profile/20') //TODO: (1) Add in actual profile number, scaffold profiles
-        .expect(200, done); //TODO: Add in check of return value
-    });
+//Special login procedure allowing the passing of sessions id through cookies
+var logIn = function(callback, done) {
+  var agent = request.agent(server);
 
-    it('should respond with no profile and a 404 status', function (done) {
-      request(server)
-        .get('/api/profile/nothingtofind')
-        .expect(404, done);
+  agent
+    .post('/api/signin')
+    .send({username: 'John', password: 'test'})
+    .end(function (err, res) {
+      if(err) {
+        done(err);
+      } else {
+        callback(agent, res);
+        done();
+      }
     });
+};
+
+describe('API - Vote', function () {
+  it('Should return 200 when posting to /api/vote, while logged in', function(done) {
+    var checkingAuthorization = function (agent, res) {
+      console.log('Accessing secured resources while signed into service');
+      agent
+        .post('/api/vote')
+        .expect(200);
+    };
+
+    logIn(checkingAuthorization, done);
+
   });
 
-  describe('GET api/browse/', function () {
-    it('should respond with a list of profile IDs/names and a 200 status', function (done) {
+  it('Should return 401 when POST/GET to /api/vote when not logged in', function (done) {
+    request(server)
+      .post('/api/vote')
+      .end(function (err, res) {
+        if(err) {
+          console.log('Error in test: ', err);
+        } else {
+          expect(res.statusCode).to.equal(401);
+          request(server)
+            .get('/api/vote')
+            .expect(401, done);
+        }
+      });
+
+  });
+});
+
+describe('API - Profile', function() {
+  describe('GET api/profile/:id', function () {
+    it('should respond with a profile object and a 200 status when logged in', function (done) {
+      var retrieveProfile = function(agent, res) {
+        console.log('Accessing profile by ID while signed into service');
+        expect(res.statusCode).to.equal(200);
+        agent
+          .get('/api/profile/1')
+          .end(function(err, res) {
+            if(err) {
+              console.log('Error in test: ', err);
+            } else {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body.id).to.equal(1);
+              expect(res.body.username).to.equal('John');
+            }
+          });
+      };
+
+      logIn(retrieveProfile, done);
+    });
+
+    it('should respond with 404 and no profile object for unknown user when logged in', function (done) {
+      var retrieveProfile = function(agent, res) {
+        console.log('Accessing profile by ID while signed into service');
+        expect(res.statusCode).to.equal(200);
+        agent
+          .get('/api/profile/1000')
+          .end(function(err, res) {
+            if(err) {
+              console.log('Error in test: ', err);
+            } else {
+              expect(res.statusCode).to.equal(404);
+              expect(res.body.id).to.be.undefined;
+              expect(res.body.username).to.be.undefined;
+              // expect(res.body.username).to.equal('John');
+            }
+          });
+      };
+
+      logIn(retrieveProfile, done);
+    });
+
+    it('Should respond with a 401 when POST/GET api/profile/anyid# when not logged in', function(done) {
+      request(server)
+        .get('/api/profile/1')
+        .end(function (err, res) {
+          if(err) {
+            console.log('Error in test: ', err);
+          } else {
+            expect(res.statusCode).to.equal(401);
+            request(server)
+              .get('/api/profile/1000')
+              .expect(401, done);
+          }
+        });
+      });
+  });
+
+  describe('API - Browse', function () {
+
+    it('should respond with a list of profile IDs and a 200 status when logged in', function (done) {
+      var retrieveProfile = function(agent, res) {
+        console.log('Accessing browse while signed into service');
+        expect(res.statusCode).to.equal(200);
+        agent
+          .get('/api/browse')
+          .end(function(err, res) {
+            if(err) {
+              console.log('Error in test: ', err);
+            } else {
+              expect(res.statusCode).to.equal(200);
+              expect(res.body[0].id).to.equal(1);
+              expect(res.body[1].id).to.be.an('number');
+            }
+          });
+      };
+      logIn(retrieveProfile, done);
+    });
+
+    it('Should respond with a 401 when GET request sent to api/browse when not logged in', function(done) {
       request(server)
         .get('/api/browse')
-        .expect(200, done); //TODO: Add in check of return value
-    });
-  });
-
-  describe('POST api/signin', function () {
-    it('should respond with 200 status with correct credentials', function (done) {
-      request(server)
-        .post('/api/signin') //TODO: Add in data
-        .expect(200, done);
-    });
-
-    xit('should respond with a 401 status with incorrect credentials', function (done) {
-      request(server)
-        .post('/api/signin') //TODO: Add in data
         .expect(401, done);
-    });
-  });
-  
-  describe('POST api/signout', function () {
-    it('should receive and process a POST for signing out', function (done) {
-      request(server)
-        .post('/api/signout')
-        .expect(200, done);
-    });
-  });
+      });
 
-  describe('POST api/user/create', function () {
-   it('should receive and process a POST request to create a new user', function (done) {
-      request(server)
-        .post('/api/user/create')
-        .expect(200, done); //TODO: Create additional test to vet the incoming request object
-   });
- });
-
-  describe('POST api/vote', function () {
-    it('should recieve and process a POST request for voting', function (done) {
-      request(server)
-        .post('/api/vote/')
-        .expect(200, done); //TODO  Create additional test to look within request object
-    });
   });
 });
--- test/test.js
+++ test/test.js
@@ -1,3 +1,4 @@
 console.log("Beginning tests");
 require('./server/apiSpec.js');
 require('./server/dbSpec.js');
+require('./server/authSpec.js');
