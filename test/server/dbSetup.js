779675a8765efefde1f2541f7c36716927d2e799
Aaron Kandle <akandle@gmail.com>
2015-12-29 16:43:06 -0800

    Travis....................

--- test/server/dbSetup.js
+++ test/server/dbSetup.js
@@ -4,7 +4,7 @@ var bcrypt = require('bcrypt');
 //Create base database setup, with one user
 console.log('==============Creating initial empty database==================');
 var db = require('../../DB/db.js');
-var profileModel = require('../../server/models/profileModel');
+var Profile = require('../../server/models/profileModel');
 var locationModel = require('../../server/models/locationModel');
 var traitModel = require('../../server/models/traitModel');
 var voteModel = require('../../server/models/voteModel');
@@ -12,5 +12,5 @@ var voteModel = require('../../server/models/voteModel');
 console.log('==============Adding user: John, with pass "test" ==================');
 var passHash = bcrypt.hashSync('test', 8);
 
-User.create({ username : 'John',
+Profile.create({ username : 'John',
               password: passHash});
