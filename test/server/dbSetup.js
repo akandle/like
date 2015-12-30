7f32d099b71d5c684e65fc671f50e4b3f406f35f
Aaron Kandle <akandle@gmail.com>
2015-12-29 16:39:11 -0800

    Pivoting approach to abuse Travis into submission

---
+++ test/server/dbSetup.js
@@ -0,0 +1,16 @@
+var Sequelize = require('sequelize');
+var bcrypt = require('bcrypt');
+
+//Create base database setup, with one user
+console.log('==============Creating initial empty database==================');
+var db = require('../../DB/db.js');
+var profileModel = require('../../server/models/profileModel');
+var locationModel = require('../../server/models/locationModel');
+var traitModel = require('../../server/models/traitModel');
+var voteModel = require('../../server/models/voteModel');
+
+console.log('==============Adding user: John, with pass "test" ==================');
+var passHash = bcrypt.hashSync('test', 8);
+
+User.create({ username : 'John',
+              password: passHash});
--- test/test.js
+++ test/test.js
@@ -1,4 +1,8 @@
-console.log("Beginning tests");
+console.log("Beginning tests ------------------------------------------------------------------");
 require('./server/apiSpec.js');
+console.log("Starting DB Spec -----------------------------------------------------------------");
+
 require('./server/dbSpec.js');
+
+console.log("Starting Auth Spec ---------------------------------------------------------------");
 require('./server/authSpec.js');
